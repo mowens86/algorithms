8ea7e5bcc4c5863041abbc45c56cffa7f458bb52
Mike Owens <mocoding86@gmail.com>
2021-01-24 11:42:56 -0800

    solve find the unique number

---
+++ codewars/find-the-unique-number.js
@@ -0,0 +1,22 @@
+const findUniq = (arr) => {
+    // do magic
+    return arr.sort((a,b) => b - a).slice(0,1)[0];
+  };
+
+  /**
+   * There is an array with some numbers. All numbers
+   *  are equal except for one. Try to find it!
+
+    findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+    findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+    It’s guaranteed that array contains at least 3 numbers.
+
+    The tests contain some very huge arrays, so think about performance.
+
+    This is the first kata in series:
+
+    Find the unique number (this kata)
+    Find the unique string
+    Find The Unique
+    FUNDAMENTALSALGORITHMSNUMBERSARRAYS
+   */
