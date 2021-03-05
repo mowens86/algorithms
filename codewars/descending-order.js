86675cc58b18017033c01b348d6150bc5c0e4a1a
Mike Owens <mocoding86@gmail.com>
2021-03-04 17:40:35 -0800

    solve descending order algorithm

---
+++ codewars/descending-order.js
@@ -0,0 +1,18 @@
+const descendingOrder = (n) => {
+    const newArr = n.toString().split('').sort((a,b) => b - a).map(item => parseInt(item)).join('');
+    return parseInt(newArr);
+  };
+
+  /**
+   * Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+    Examples:
+    Input: 42145 Output: 54421
+
+    Input: 145263 Output: 654321
+
+    Input: 123456789 Output: 987654321
+
+    FUNDAMENTALSFUNCTIONSCONTROL FLOWBASIC LANGUAGE FEATURES
+   * 
+   */
