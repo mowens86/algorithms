954feae632b460bdb431f40a8be8ef8e8e76d916
Mike Owens <mocoding86@gmail.com>
2021-05-01 08:48:57 -0700

    solve average scores algorithm

---
+++ codewars/7 kyu/average-scores.js
@@ -0,0 +1,7 @@
+const average = (scores) => Math.round(scores.reduce((a, b) => (a + b)) / scores.length);
+/**
+ * Create a function that returns the average of an array of numbers ("scores"), rounded to
+ *  the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+    FUNDAMENTALSMATHEMATICSALGORITHMSNUMBERS
+ */
