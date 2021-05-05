79ec5342c0d5009df4460d8c3030f6d3c7b25e4e
Mike Owens <mocoding86@gmail.com>
2021-05-04 20:03:21 -0700

    solve beginner series #3 sum opf numbers algorithm

---
+++ codewars/7 kyu/beginner-series#3-sum-of-numbers.js
@@ -0,0 +1,35 @@
+const getSum = ( a,b ) => {
+    let counter = a;
+    let sum = 0;
+    if (a === b) return a;
+    if (a <= b) {
+      while (counter <= b ) {
+        sum += counter;
+        counter++;
+      }
+    }
+    
+    if (a >= b) {
+      while (counter >= b ) {
+        sum += counter;
+        counter--;
+      }
+    }
+    return sum;
+  }
+
+  /**
+   * Given two integers a and b, which can be positive or negative, find the sum of all the integers
+   *  between including them too and return it. If the two numbers are equal return a or b.
+
+    Note: a and b are not ordered!
+
+    Examples
+    GetSum(1, 0) == 1   // 1 + 0 = 1
+    GetSum(1, 2) == 3   // 1 + 2 = 3
+    GetSum(0, 1) == 1   // 0 + 1 = 1
+    GetSum(1, 1) == 1   // 1 Since both are same
+    GetSum(-1, 0) == -1 // -1 + 0 = -1
+    GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+    FUNDAMENTALSALGORITHMS
+   */
