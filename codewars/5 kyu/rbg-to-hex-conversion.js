35df2fcc050d9adc4bed17c40b63b4397906fe7e
Mike Owens <mocoding86@gmail.com>
2021-05-09 10:17:55 -0700

    solve rgb to hex conversion algorithm

---
+++ codewars/5 kyu/rbg-to-hex-conversion.js
@@ -0,0 +1,35 @@
+const rgb = (r, g, b) => {
+    if (r > 255) r = 255;
+    if (r < 0) r = 0;
+    if (b > 255) b = 255;
+    if (b < 0) b = 0;
+    if (g > 255) g = 255;
+    if (g < 0) g = 0;
+    
+    r = r.toString(16);
+    b = b.toString(16);
+    g = g.toString(16);
+    
+    if (r.length == 1) r = `0${r}`;
+    if (b.length == 1) b = `0${b}`;
+    if (g.length == 1) g = `0${g}`;
+    
+    const hexColors = (r + g + b).toUpperCase();
+    return hexColors;
+  };
+
+  /**
+   * The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal
+   *  representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that
+   *  range must be rounded to the closest valid value.
+
+    Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+    The following are examples of expected output values:
+
+    rgb(255, 255, 255) // returns FFFFFF
+    rgb(255, 255, 300) // returns FFFFFF
+    rgb(0,0,0) // returns 000000
+    rgb(148, 0, 211) // returns 9400D3
+    ALGORITHMS
+   */
