5704fab4c2cb7bfaf6bb94bf3ebcefcf48043561
mocoding86 <mocoding86@gmail.com>
2021-06-26 22:59:07 -0700

    solve gravity flip

---
+++ codewars/8 kyu/get-the-mean-of-an-array.js
@@ -0,0 +1,18 @@
+const getAverage = (marks) => {
+    //TODO : calculate the downward rounded average of the marks array
+    const lengthOfNums = marks.length;
+    const reducedArr = marks.reduce((acc, curr) => acc + curr);
+    return Math.floor(reducedArr / lengthOfNums);
+  };
+
+  /**
+   * It's the academic year's end, fateful moment of your school report.
+   *  The averages must be calculated. All the students come to you and
+   *  entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+    Return the average of the given array rounded down to its nearest integer.
+
+    The array will never be empty.
+
+    ALGORITHMSARRAYS
+   */
