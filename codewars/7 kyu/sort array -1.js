187599bda8c8efad60633830ca22812a1cd86db5
mocoding86 <mocoding86@gmail.com>
2021-07-08 19:55:32 -0700

    solve sort arrays -1

---
+++ codewars/7 kyu/sort array -1.js
@@ -0,0 +1,7 @@
+const sortme = ( names ) => names.sort();
+
+/**
+ * Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+FUNDAMENTALSSORTINGALGORITHMSARRAYS
+ */
