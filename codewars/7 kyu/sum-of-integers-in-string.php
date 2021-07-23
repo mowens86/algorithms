91a8de972f8bd93ff9d5daf025949389011f4575
mocoding86 <mocoding86@gmail.com>
2021-07-22 19:45:18 -0700

    solve sum of string integers

---
+++ codewars/7 kyu/sum-of-integers-in-string.php
@@ -0,0 +1,19 @@
+<?php 
+
+function sum_of_integers_in_string(string $s): int {
+    preg_match_all('!\d+!', $s, $matches);
+    $number = implode(' ', $matches[0]);
+    $numberArr = explode(' ', $number);
+    return array_sum($numberArr);
+  };
+
+  /**
+   * Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+    Note: only positive integers will be tested.
+
+    FUNDAMENTALS
+   */
+
+
+?>
