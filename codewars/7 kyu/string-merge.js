16edf26bbb67f14ae6bb8ccd33323b39613d1967
Mike Owens <mocoding86@gmail.com>
2022-01-20 20:04:12 -0800

    solve string merge 7 kyu algorithm

---
+++ codewars/7 kyu/string-merge.js
@@ -0,0 +1,16 @@
+const stringMerge = (string1, string2, letter) => {
+    const indexOne = string1.indexOf(letter);
+    const indexTwo = string2.indexOf(letter);
+    return string1.substring(0,indexOne).concat(string2.substring(indexTwo, string2.length));
+  };
+
+  /**
+   * Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+
+    Examples
+    ("hello", "world", "l")       ==>  "held"
+    ("coding", "anywhere", "n")   ==>  "codinywhere"
+    ("jason", "samson", "s")      ==>  "jasamson"
+    ("wonderful", "people", "e")  ==>  "wondeople"
+    FUNDAMENTALSSTRINGSGAMES
+   */
